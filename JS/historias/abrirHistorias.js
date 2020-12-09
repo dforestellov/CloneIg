8bb5e427fedb303cc8d466b6bc622d99983324f6
emilianommdavila <emilianommdavila@gmail.com>
2020-12-09 14:31:09 -0300

    historias

--- JS/historias/abrirHistorias.js
+++ JS/historias/abrirHistorias.js
@@ -1,31 +1,26 @@
-const funcionMostrar = () => {
+const funcionMostrar = (soyElNumero) => {
+  numeroDelArray = soyElNumero.path[2].dataset.identificador;
+
   const lugarParaPonerHistorias = document.querySelector(
     "#lugarParaPonerHistorias"
   );
-
-  const historia = document.querySelector("#historia");
-  historia.addEventListener("click", mostrarHistoria);
-
   const historiaNueva = (foto) => {
     return `
-    <div class="cosoNuevo">
-    <img class="fotoHistoria" id="historiaNueva" src="${foto}" alt="">
-</div>
-`;
+          <div class="cosoNuevo">
+          
+          <img class="fotoHistoria" id="historiaNueva" src="${foto}" alt="">
+      </div>
+      `;
   };
-
   const insertarHistoria = (historiaParaMostrar) => {
     const insertarHistoria = anadirHistoria(historiaParaMostrar);
     lugarParaPonerHistorias.appendChild(insertarHistoria);
   };
-
   const anadirHistoria = (foto) => {
     const elemento = document.createElement("history");
     elemento.innerHTML = historiaNueva(foto);
+
     return elemento;
-  };
-  const map_function = () => {
-    return;
   };
 
   function mostrarHistoria() {
@@ -40,6 +35,7 @@ const funcionMostrar = () => {
     function cambiarSrc(src) {
       historiaSeleccionada = document.querySelector("#historiaNueva");
       historiaSeleccionada.src = src;
+      console.log("cambie");
     }
 
     function ajaxCallback() {
@@ -47,10 +43,12 @@ const funcionMostrar = () => {
         const respuestaAjax = ajax.response;
         const respuestaParseada = JSON.parse(respuestaAjax);
         const publicaciones = respuestaParseada.data;
-        let a = -1;
+        let a = numeroDelArray - 2;
         bucleHistorias();
-        setInterval(cambiarHIstoria, 1000);
+        console.log(publicaciones.length);
 
+        let intervalo = setInterval(cambiarHistoria, 3000);
+        intervalo;
         function bucleHistorias() {
           a++;
 
@@ -59,14 +57,24 @@ const funcionMostrar = () => {
           insertarHistoria(arrayFotos);
         }
 
-        function cambiarHIstoria() {
-          a++;
+        function cambiarHistoria() {
+          if (publicaciones.length > a) {
+            a++;
+            console.log(a);
+            console.log(publicaciones.length);
 
-          const arrayFotos = publicaciones[a].historia;
-          console.log(arrayFotos);
-          cambiarSrc(arrayFotos);
+            const arrayFotos = publicaciones[a].historia;
+
+            cambiarSrc(arrayFotos);
+          } else {
+            console.log("soy mayor");
+            clearInterval(intervalo);
+            cerrarHistorias();
+            cerrarHistorias();
+          }
         }
       }
     }
   }
+  mostrarHistoria();
 };
